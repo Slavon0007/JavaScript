90c37c8c0f87e7d8dd6b443c75425db4af39dba2
Slava <slavkaklimov@gmail.com>
2019-11-19 19:11:28 +0200

    we

--- lesson3/index.js
+++ lesson3/index.js
@@ -13,17 +13,17 @@ const l = [1, 2, 3, 4];
 const m = function run() {
     return 17;
 };
-console.log('a:', typeOf 17);
-console.log('b:', typeOf 17.17);
-console.log('c:', typeOf '17');
-console.log('d:', typeOf "17");
-console.log('e:', typeOf "I'm a string");
-console.log('f:', typeOf false);
-console.log('g:', typeOf true);
-console.log('h:', typeOf null);
-console.log('i:', typeOf undefined);
-console.log('j:', typeOf Symbol("I'm a Symbol"));
-console.log('k:', typeOf { name: 'John' });
-console.log('l:', typeOf[1, 2, 3, 4]);
-console.log('m:', typeOf
-    function);
+console.log('a:', typeof 17);
+console.log('b:', typeof 17.17);
+console.log('c:', typeof '17');
+console.log('d:', typeof "17");
+console.log('e:', typeof "I'm a string");
+console.log('f:', typeof false);
+console.log('g:', typeof true);
+console.log('h:', typeof null);
+console.log('i:', typeof undefined);
+console.log('j:', typeof Symbol("I'm a Symbol"));
+console.log('k:', typeof { name: 'John' });
+console.log('l:', typeof [1, 2, 3, 4]);
+console.log('m:', typeof
+    function run() {});
