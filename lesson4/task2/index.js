dc7f4bdfb51014aaa972d39063b53b391f0e53f0
Slava <slavkaklimov@gmail.com>
2019-11-20 20:08:18 +0200

    test2

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,10 +1,14 @@
 let m = 10;
 let n = 20;
+let i = m;
 let result = 1;
 
 do {
-    if (m % 2 === 1) {
-        result *= m;
+    if (i % 2 === 1) {
+        result *= i;
     }
-} while (n > o);
-console.log('resolt' + resolt);
+    i++;
+
+} while (i < n);
+
+console.log('result: ', result);
