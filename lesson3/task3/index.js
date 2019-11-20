c6933f2b54b7572e7109826e90b10de8911b9785
Slava <slavkaklimov@gmail.com>
2019-11-20 13:38:52 +0200

    test4

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -26,14 +26,31 @@ console.log("'ab' > 'a': ", 'ab' > 'a');
 /* строгое сравнение */
 
 17 === 1;
+console.log("17 === 1: ", 17 === 1);
+
 17 === 17;
+console.log("17 === 17: ", 17 === 17);
+
 17 === '17';
+console.log("17 === '17': ", 17 === '17');
+
 17 === true;
+console.log("17 === true: ", 17 === true);
+
 '0' === '';
+console.log("'0' === '': ", '0' === '');
+
 true === false;
+console.log("true === false: ", true === false);
+
 true === true;
+console.log("true === true: ", true === true);
+
 null === undefined;
+console.log("null === undefined: ", null === undefined);
+
 false === 0
+console.log("false === 0: ", false === 0);
 
 
 /* нестрогое сравнение */
