4aee41ff06a89e8a837d83bdc326ff3f34d95b7d
Slava <slavkaklimov@gmail.com>
2019-11-20 13:22:01 +0200

    test2

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -7,9 +7,13 @@ a > b;
 console.log("a > b: ", a > b);
 
 a <= b;
-a == b;
-a != b;
+console.log("a <= b: ", a <= b);
 
+a == b;
+console.log("a == b: ", a == b);
+
+a != b;
+console.log("a != b: ", a != b);
 
 /* сравнение строк */
 
@@ -20,3 +24,28 @@ a != b;
 
 17 === 1;
 17 === 17;
+17 === '17';
+17 === true;
+'0' === '';
+true === false;
+true === true;
+null === undefined;
+false === 0
+
+
+/* нестрогое сравнение */
+
+17 == '17';
+'0' == '';
+0 == '';
+null == undefined;
+false == 0;
+
+
+/* осторожно c null и undefined */
+
+undefined == null;
+undefined == 0;
+null == 0;
+undefined < 0;
+undefined > 0;
