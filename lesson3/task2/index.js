0cb7e5b911980cdde61b9f42e1ca2efe679222b1
Slava <slavkaklimov@gmail.com>
2019-11-20 12:09:55 +0200

    test2

--- lesson3/task2/index.js
+++ lesson3/task2/index.js
@@ -1,1 +1,6 @@
 console.log('17 to string is ' + String(17));
+console.log('-17.17 to string is ' + String(-17.17));
+console.log('false to string is ' + String(false));
+console.log('null to string is ' + String(null));
+console.log('undefined to string is ' + String(undefined));
+console.log('0 to string is ' + String(0));
