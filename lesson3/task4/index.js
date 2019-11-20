e9db3d70f984b09040599e7da1e9df610b4fad86
Slava <slavkaklimov@gmail.com>
2019-11-20 14:46:35 +0200

    test2

--- lesson3/task4/index.js
+++ lesson3/task4/index.js
@@ -10,9 +10,13 @@ console.log("false || false || false: ", false || false || false);
 
 /* оператор && (AND) */
 false && true;
-false && true && false;
-true && true && true;
+console.log("false && true: ", false && true);
 
+false && true && false;
+console.log("false && true && false: ", false && true && false);
+
+true && true && true;
+console.log("true && true && true: ", true && true && true);
 
 /* Логические операторы могут применятся к любым типам данных */
 
