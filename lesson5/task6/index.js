c59361988c7db5983d54c5a936826996af48ebca
Slava <slavkaklimov@gmail.com>
2019-11-21 15:25:54 +0200

    test1

---
+++ lesson5/task6/index.js
@@ -0,0 +1,12 @@
+function getNum(num) {
+    for (let i = 2; i <= num; i++) {
+        if (i % num === o) {
+            for (let j = 1; j <= num; j++) {
+                if (j == i) {
+                    console.log(i);
+                }
+            }
+        } else { return false; }
+    }
+
+}
