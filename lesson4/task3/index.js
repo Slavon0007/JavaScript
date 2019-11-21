d4f79fd1210c41e3ca532c96782de0c18501b212
Slava <slavkaklimov@gmail.com>
2019-11-21 10:07:06 +0200

    test2

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 5; i++) {
+for (let i = 1; i < 5; i++) {
     console.log('=>' + i + '<=');
     for (let j = 1; j < 10; j++) {
         console.log(i + ' x ' + j + ' = ' + i * j);
