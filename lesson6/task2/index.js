f88a97d1b1f8bde7d64e58094ef7eb7c58b4a4f5
Slava <slavkaklimov@gmail.com>
2019-11-22 13:00:45 +0200

    test2

---
+++ lesson6/task2/index.js
@@ -0,0 +1,8 @@
+function getSum(arr) {
+    if (!Array.isArray(arr)) return null;
+    let result = 0;
+    for (let i = 0; i <= arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
+}
