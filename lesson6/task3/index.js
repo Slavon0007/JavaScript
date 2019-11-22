e2aaec29f1a2c795608236bfac96f3f1deb1c37b
Slava <slavkaklimov@gmail.com>
2019-11-22 13:19:45 +0200

    rest1

---
+++ lesson6/task3/index.js
@@ -0,0 +1,10 @@
+function getSpecialNumbers(m, n) {
+    let arr = [];
+
+    for (let i = m; i < n; i++) {
+        if (i % 3 === 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
