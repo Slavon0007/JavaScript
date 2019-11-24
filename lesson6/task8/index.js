e05045e0962c21da46d87005acfd2faf4f29c705
Slava <slavkaklimov@gmail.com>
2019-11-24 22:34:35 +0200

    test1

---
+++ lesson6/task8/index.js
@@ -0,0 +1,18 @@
+function uniqueCount(array) {
+
+    let arr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let found = false;
+        for (let j = array[i] + 1; j < array.length - 1; j++) {
+            if (array[i] === array[j]) {
+                found = true;
+            }
+        }
+
+        if (found) {
+            arr.push(array[i]);
+        }
+    }
+    return arr.length;
+}
