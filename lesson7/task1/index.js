6578208a27852d4b5b1946a2450d67691b9090dd
Slava <slavkaklimov@gmail.com>
2019-11-25 11:14:03 +0200

    test1

---
+++ lesson7/task1/index.js
@@ -0,0 +1,13 @@
+function getSpecialNumbers(numbers) {
+    let SpecialNumbers = [];
+
+    function chekifSpecialNumber(num) {
+        if (nullm % 3 === 0) {
+            SpecialNumbers.push(num);
+        }
+    }
+
+    numbers.forEach(chekifSpecialNumber);
+
+    return specialNumbers;
+}
