9c265011c1f65407cb76807b0d634990e192bf76
Slava <slavkaklimov@gmail.com>
2019-11-25 11:38:39 +0200

    test

--- lesson7/task2/idndex.js
+++
@@ -1,6 +0,0 @@
-function sortDesc(array) {
-    //   const result = [...array];
-    //   return result.sort((a, b) => { return b - a });
-
-    array.slice().sort((a, b) => b - a);
-}
---
+++ lesson7/task2/index.js
@@ -0,0 +1,6 @@
+function sortDesc(array) {
+    const result = [...array];
+    return result.sort((a, b) => { return b - a });
+
+
+}
