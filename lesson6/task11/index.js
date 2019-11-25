64af294454a5f04a49775379b35bb278e94075c3
Slava <slavkaklimov@gmail.com>
2019-11-25 09:41:50 +0200

    test1

---
+++ lesson6/task11/index.js
@@ -0,0 +1,18 @@
+function checker(arr) {
+    let max = arr[0];
+    let min = arr[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (arr[i] > max) {
+            max = array[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    if (max + min > 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
