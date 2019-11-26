11347aa7a65f007ac5c8410a40a73345a2c751cb
Slava <slavkaklimov@gmail.com>
2019-11-26 13:42:32 +0200

    test

--- lessson9/task3/index.js
+++ lessson9/task3/index.js
@@ -13,7 +13,7 @@
 
 const getCustomersList = obj => Object.entries(obj)
     .map(elem => ({ id: elem[0], ...obj[elem[0]] }))
-    .filter((elem1, elem2) => elem1.age - elem2.age);
+    .sort((elem1, elem2) => elem1.age - elem2.age);
 
 
 //  [
