df6681c6d770404fd7c0fb2daf74eb50a66faada
Slava <slavkaklimov@gmail.com>
2019-11-26 16:05:08 +0200

    test

--- lesson10/task2/index.js
+++ lesson10/task2/index.js
@@ -2,6 +2,6 @@ const getParsedIntegers = arr => arr.map(elem => Number.parseInt(elem));
 
 const getParsedIntegersV2 = arr => arr.map(elem => parseInt(elem));
 
-const getParsedFloats = arr => arr.map(ele => Number.parseFloat(elem));
+const getParsedFloats = arr => arr.map(elem => Number.parseFloat(elem));
 
 const getParsedFloatsV2 = arr => arr.map(elem => parseFloat(elem));
