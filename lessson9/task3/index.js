2122e74331fe090a6df5f97b3990b812738f0730
Slava <slavkaklimov@gmail.com>
2019-11-26 13:36:29 +0200

    test

--- lessson9/task3/index.js
+++ lessson9/task3/index.js
@@ -1,3 +1,31 @@
+//const customers = {
+//    'customer-id-1': {
+//        name: 'William',
+//        age: 54
+//    },
+//    'customer-id-2': {
+//        name: 'Tom',
+//        age: 17
+//    },
+//    ...
+//};
+
+
 const getCustomersList = obj => Object.entries(obj)
-    .map(elem => ({ id: elem[1], ...obj[elem[1]] }))
+    .map(elem => ({ id: elem[0], ...obj[elem[0]] }))
     .filter((elem1, elem2) => elem1.age - elem2.age);
+
+
+//  [
+//      {
+//          name: 'Tom',
+//          age: 17,
+//          id: 'customer-id-2'
+//      },     
+//{
+//          name: 'William',
+//          age: 54,
+//          id: 'customer-id-1'
+//      },
+//      ...
+//  ]
