91f76eb5154f7c37b47c2f86497162009dd7cd50
Slava <slavkaklimov@gmail.com>
2019-11-29 21:22:12 +0200

    Lesson 13 task 2

---
+++ lesson13/task2/index.test.js
@@ -0,0 +1,18 @@
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
+
+it('shoud get square numbers', () => {
+    const result = getSquaredArray([1, 2, 3]);
+    expect(result).toEqual([1, 4, 9]);
+});
+
+it('shoud keep odd numbers only', () => {
+    const result = getOddNumbers([1, 2, 3, 4, 5]);
+
+    expect(result).toEqual([1, 3, 5]);
+});
+
+it('shoud get sum of numbers', () => {
+    const result = getSum(8, 4);
+
+    expect(result).toEqual(12);
+});
