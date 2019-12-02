9eb816e1d6bb351b63611cb646537b6ba793a660
Slava <slavkaklimov@gmail.com>
2019-12-02 18:59:50 +0200

    function calc

---
+++ lesson15/hw1/index.js
@@ -0,0 +1,22 @@
+function createCalculator() {
+
+    let result = 0;
+
+    return {
+        add: function(num) {
+            result += num;
+        },
+
+        decrease: function(num) {
+            result -= num;
+        },
+
+        reset: function() {
+            result = 0;
+        },
+
+        getMemo: function() {
+            return result;
+        },
+    };
+}
