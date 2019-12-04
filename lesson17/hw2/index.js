ab7d170f52fcd42ba46f620845508de512a8b600
Slava <slavkaklimov@gmail.com>
2019-12-04 20:40:35 +0200

    fixt

--- lesson17/hw2/index.js
+++ lesson17/hw2/index.js
@@ -1,21 +1,21 @@
 const timer = {
     secondsPassed: 0,
     minsPassed: 0,
-    clearIntervalID: 0,
-    setTime() {
+    intervalId: 0,
+    getTime() {
         return `${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
     },
     startsTimer() {
-        this.clearIntervalID = setInterval(() => {
+        this.intervalId = setInterval(() => {
             this.secondsPassed += 5
             if (this.secondsPassed === 60) {
                 this.minsPassed += 1;
                 this.secondsPassed = 0;
             }
-        }, 50);
+        }, 5000);
     },
     stopTimer() {
-        clearInterval(this.clearIntervalID);
+        clearInterval(this.intervalId);
     },
     resetTimer() {
         this.minsPassed = 0;
