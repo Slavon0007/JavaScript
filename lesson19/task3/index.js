6587ad6f520cd4169dac04488b5c82d947170143
Slava <slavkaklimov@gmail.com>
2019-12-09 08:01:44 +0200

    user

---
+++ lesson19/task3/index.js
@@ -0,0 +1,14 @@
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(fullName) {
+        const [firstName, lastName] = fullName.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+};
+
+export default user;
