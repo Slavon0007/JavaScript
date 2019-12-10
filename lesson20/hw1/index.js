3a76ebc9db497d47d42b36c04170522a1f74a459
Slava <slavkaklimov@gmail.com>
2019-12-10 11:29:19 +0200

    fixed problem

--- lesson20/hw1/index.js
+++ lesson20/hw1/index.js
@@ -1,34 +1,31 @@
 class Order {
 
     constructor(price, city, type) {
-
         this.id = `${Math.random()}`;
         this.dateCreated = new Date();
         this.isConfirmed = false;
-        this.dateConfimed = null;
+        this.dateConfirmed = null;
         this.price = price;
         this.city = city;
         this.type = type;
-
     }
-    checkPrice() {
 
+    checkPrice() {
         if (this.price < 1000) return false;
         return true;
-
     }
+
     confirmOrder() {
-
         this.isConfirmed = true;
-        this.dateConfimed = new Date();
+        this.dateConfirmed = new Date();
 
+        return this.confirmOrder;
     }
+
     isValidType() {
-        if (this.type == 'Buy' || this.type == 'Sell') return true;
+        if (this.type === 'Buy' || this.type === 'Sell') return true;
         return false;
     }
-}
-
-export {
-    Order
 };
+
+export { Order };
