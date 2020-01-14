f3f2981f89f434564b88581093cd3200c6e1c71f
Slava <slavkaklimov@gmail.com>
2020-01-14 19:37:32 +0200

    fibonachi

---
+++ lesson28/HW2/index.js
@@ -0,0 +1,15 @@
+const fibonacci = num => {
+    if (num < 1) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+};
+export const maxFibonacci = value => {
+    let index = 0;
+    let maxFib = 0;
+    while (true) {
+        let fib = fibonacci(index);
+        if (fib < value) {
+            maxFib = fib;
+            index++;
+        } else return maxFib;
+    }
+};
