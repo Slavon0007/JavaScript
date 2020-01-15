f5bd88f4b5535d4114789110b7f67652f012d2f6
Slava <slavkaklimov@gmail.com>
2020-01-15 18:00:39 +0200

    rrrr

---
+++ lesson29/task1/index.js
@@ -0,0 +1,19 @@
+export const addImage = (imgSrc, callback) => {
+    const imgElem = document.createElement("img");
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+    const onImageLoaded = () => callback(null, { width: 200, height: 100 });
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+
+    callback = (error) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+    };
+
+};
