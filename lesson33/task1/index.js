a7462bc05ca67cf32a591182a00caf72be4eda5c
Slava <slavkaklimov@gmail.com>
2020-01-21 17:55:50 +0200

    fitch

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -5,30 +5,24 @@ const userLocationElem = document.querySelector('.user__location');
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
 
-
-const fetchUsarData = userName => {
-    return fetch(`https://api.github.com/user/${userName}`)
+const fetchUserData = userName => {
+    return fetch(`https://api.github.com/users/${userName}`)
         .then(response => response.json());
-}
-const renderUserData = () => {
-    const { avatar_upl, name, location } = userData;
-    userAvatarElem.src = avatar_upl;
+};
+
+const renderUserData = userData => {
+    const { avatar_url, name, location } = userData;
+    userAvatarElem.src = avatar_url;
     userNameElem.textContent = name;
-    userLocationElem.textContent = location ?
-        `from ${location}` :
-        '';
+    userLocationElem.textContent = location ? `from ${location}` : '';
 };
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
-const userNameInputElem = document.querySelector('.name-form__input');
-
-
+const userNameInputEelem = document.querySelector('.name-form__input');
 
 const onSearchUser = () => {
-    const userName = userNameInputElem.value;
-    fetchUsarData(userName)
+    const userName = userNameInputEelem.value;
+    fetchUserData(userName)
         .then(userData => renderUserData(userData));
-
 };
-
 showUserBtnElem.addEventListener('click', onSearchUser);
