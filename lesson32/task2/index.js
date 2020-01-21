ab392dd4a46be04bac1d649cab03214c4891f968
Slava <slavkaklimov@gmail.com>
2020-01-21 12:05:44 +0200

    test Nan

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -1,37 +1,31 @@
-const getRandomNumber = (from, to) => {
+const getRandomNumber = (from, to) =>
     from + Math.random() * (to - from);
-}
-
 const request = url => new Promise(resolve => {
+    const randomDelay = getRandomNumber(1000, 3000);
     setTimeout(() => {
-        const randomDelay = getRandomNumber(1000, 3000);
         resolve({
             userData: {
                 name: 'Tom',
-                age: '17',
+                age: 17,
             },
             source: url,
         });
     }, randomDelay);
 });
 
-
 const servers = [
     'https://server.com/us',
     'https://server.com/eu',
     'https://server.com/au',
 ];
-
-
 export const getUserASAP = userId => {
     const userUrls = servers
-        .map(serverUrl => `${serverUrl}/user/${userId}`);
+        .map(serverUrl => `${serverUrl}/users/${userId}`);
 
-    const request = userUrls
-        .map(userUrls => request(userUrls));
+    const requests = userUrls
+        .map(userUrl => request(userUrl));
 
-    return Promise.race(request);
+    return Promise.race(requests);
 };
-
 getUserASAP('user-id-1')
     .then(res => console.log(res));
