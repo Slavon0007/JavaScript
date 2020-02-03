dde224d69077f83ab7e9d5896cccda9c05ccd9e2
Slava <slavkaklimov@gmail.com>
2020-02-03 15:09:23 +0200

    async/await

---
+++ lesson36/task1/index.js
@@ -0,0 +1,11 @@
+const getUser = async userId => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userId}`);
+        if (!response.ok) {
+            return null;
+        }
+        return userData = await response.json();
+    } catch (err) {
+        throw new Error('Faild to fetch user');
+    }
+};
